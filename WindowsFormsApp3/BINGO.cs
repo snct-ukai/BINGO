95ad4c7c8f4472fab129a71f2bf9661fa366c865
snct-ukai <73654679+snct-ukai@users.noreply.github.com>
2020-12-09 23:34:57 +0900

    プロジェクト ファイルを追加します。

---
+++ WindowsFormsApp3/BINGO.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class BINGO : Form
+    {
+        Random rnd = new System.Random();
+        int[] s = new int[100];
+        int i = 0;
+        int l = 0;
+        public BINGO()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (i > 74)
+            {
+                Environment.Exit(0);
+            }
+            int num = 0;
+            bool n = false;
+            do
+            {
+                int r = rnd.Next(1, 76);
+                n = false;
+                for (int j = 0; j < i + 1; j++)
+                {
+                    if (r == s[j])
+                    {
+                        n = true;
+                    }
+                    num = r;
+                }
+            } while (n);
+            s[i] = num;
+            label1.Text += (s[i].ToString());
+            label1.Text += "   ";
+            i++;
+            l++;
+            if (l == 28)
+            {
+                label1.Text += '\n';
+                label1.Text += '\n';
+                l = 0;
+            }
+            label2.Text = (i.ToString());
+            label2.Text += "個目";
+        }
+
+        private void BINGO_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
