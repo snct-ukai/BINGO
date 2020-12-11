6a987c054eaf85aa09fa52c585cd11bdea5b9057
鵜飼 笑太 1I10 <r02i08@suzuka.kosen-ac.jp>
2020-12-11 17:18:26 +0900

    コメントを入れただけ

--- WindowsFormsApp3/BINGO.cs
+++ WindowsFormsApp3/BINGO.cs
@@ -15,6 +15,7 @@ namespace WindowsFormsApp3
     public partial class BINGO : Form
     {
         /*
+        FHD
         label1.Fontsize=46
         label2.Fontsize=52
         label3.Fontsize=16
@@ -37,25 +38,30 @@ namespace WindowsFormsApp3
             {
                 label4.Text = "0人";
             }
+            //75個目の乱数を表示し終わったあとクリックされたときメモリエラーを防ぐため強制終了させる
             if (i > (bingo - 1))
             {
                 Environment.Exit(0);
             }
             int num = 0;
-            bool n = false;
+            bool n;
             do
             {
+                //1からconst int bingoの値までの乱数を生成
                 int r = rnd.Next(1, (bingo+1));
                 n = false;
                 for (int j = 0; j < i + 1; j++)
                 {
+                    //もし今までに出たのならbool nをtrueにし、乱数を生成し直す(配列で評価)
                     if (r == s[j])
                     {
                         n = true;
                     }
+                    //int numに生成した乱数を代入する
                     num = r;
                 }
             } while (n);
+            //配列のi番目に生成した乱数を入れる
             s[i] = num;
             label1.Text += (s[i].ToString());
             label1.Text += "    ";
@@ -63,7 +69,7 @@ namespace WindowsFormsApp3
             label2.Text = (i.ToString());
             label2.Text += "個目";
             label3.Text = "残り";
-            label3.Text += ((bingo - i).ToString());
+            label3.Text += ((bingo - i).ToString())+"個";
         }
 
         private void BINGO_Load(object sender, EventArgs e)
